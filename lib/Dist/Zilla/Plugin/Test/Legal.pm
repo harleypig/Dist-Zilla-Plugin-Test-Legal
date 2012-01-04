817be911a344ec49ab60f1a85466dfc2c2960a4c
Harley Pig <harleypig@gmail.com>
2012-01-03 20:02:34 -0700

    initial add of files

---
+++ lib/Dist/Zilla/Plugin/Test/Legal.pm
@@ -0,0 +1,39 @@
+package Dist::Zilla::Plugin::Test::Legal;
+
+# ABSTRACT: common tests to check for copyright and license notices
+
+=head1 SYNOPSIS
+
+in C<dist.ini>
+
+  [Test::Legal]
+
+=head1 DESCRIPTION
+
+This module will add a L<Test::Legal> test as a release test to your module
+
+=cut
+
+use strict;
+use warnings;
+
+use Moose;
+extends 'Dist::Zilla::Plugin::InlineFiles';
+
+our $VERSION = 0.01;
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+__DATA__
+__[ xt/release/test-legal.t ]__
+
+use strict;
+use warnings;
+use Test::More;
+use Test::Requires { 'Test::Legal' };
+
+copyright_ok;
+license_ok;
+done_testing;
