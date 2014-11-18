1ef0475ded897479d3a36e663dbd1fc529c29ea1
Harley Pig <harleypig@gmail.com>
2014-11-18 15:20:59 +0000

    made generated file check for Test::Legal and skip instead of die

--- lib/Dist/Zilla/Plugin/Test/Legal.pm
+++ lib/Dist/Zilla/Plugin/Test/Legal.pm
@@ -14,13 +14,16 @@ This module will add a L<Test::Legal> test as a release test to your module
 
 =cut
 
+use 5.006;
 use strict;
 use warnings;
+
+use namespace::autoclean;
 
 use Moose;
 extends 'Dist::Zilla::Plugin::InlineFiles';
 
-our $VERSION = 0.01;
+# VERSION
 
 __PACKAGE__->meta->make_immutable;
 
@@ -28,11 +31,14 @@ __PACKAGE__->meta->make_immutable;
 
 __DATA__
 __[ xt/release/test-legal.t ]__
-
 use strict;
 use warnings;
+
 use Test::More;
-use Test::Requires 'Test::Legal';
+
+eval 'use Test::Legal';
+plan skip_all => 'Test::Legal required for testing licenses'
+  if $@
 
 copyright_ok;
 license_ok;
